8da4903a0085df60443cb8ecc5f11c994faa9cf5
GOPIKAPANDI <gopikapandi.r2021csea@sece.ac.in>
2024-01-21 16:24:36 +0530

    finalsmfe

--- src/api/PostRequest.js
+++ src/api/PostRequest.js
@@ -1,36 +1,6 @@
-// // import axios from "axios"
-
-// // const API = axios.create({ baseURL: "http://localhost:5000" });
-
-// // export const getTimelinePosts = (id) => API.get(`/posts/${id}/timeline`)
-
-// // PostRequest.js
-// import axios from "axios";
-
-// const API = axios.create({ baseURL: "http://localhost:5000" });
-
-// export const getTimelinePosts = (id) => async (dispatch) => {
-//   dispatch({ type: "RETREIVING_START" });
-//   try {
-//     const { data } = await API.get(`/posts/${id}/timeline`);
-//     dispatch({ type: "RETREIVING_SUCCESS", data: data });
-//   } catch (error) {
-//     console.log(error);
-//     dispatch({ type: "RETREIVING_FAIL" });
-//   }
-// };
-
-// import axios from "axios";
-
-// const API = axios.create({ baseURL: "http://localhost:5000" });
-
-// export const getTimelinePostsRequest = (id) => {
-//   return API.get(`/posts/${id}/timeline`);
-// };
-
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://socialmediarestapi.onrender.com' });
 
 export const getTimelinePosts= (id)=> API.get(`/posts/${id}/timeline`); 
 export const likePost = (id , userId) => API.put(`posts/${id}/like`, {userId : userId}) 
