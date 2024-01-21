3eb15fb9fb3c4ae812370bf108fb2be65e2882df
GOPIKAPANDI <gopikapandi.r2021csea@sece.ac.in>
2024-01-21 19:17:37 +0530

    smfe

--- src/api/AuthRequest.js
+++ src/api/AuthRequest.js
@@ -2,6 +2,7 @@ import axios from "axios"
 
 const API = axios.create({baseURL : "https://socialmediarestapi4.onrender.com"}); 
 
+
 export const logIn = (formData) => API.post('/auth/login',formData)   
 export const signUp = (formData) => API.post('/auth/register',formData)  
 
--- src/api/UploadRequest.js
+++ src/api/UploadRequest.js
@@ -2,5 +2,6 @@ import axios from "axios"
 
 const API = axios.create({baseURL : "https://socialmediarestapi4.onrender.com"}); 
 
+
 export const uploadImage = (data) => API.post('/upload/',data) 
 export const uploadPost = (data) => API.post('/posts',data)  
