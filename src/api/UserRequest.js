92d39fd8fd3d4fb51b0796700bbd8ec55e406d3e
GOPIKAPANDI <gopikapandi.r2021csea@sece.ac.in>
2024-01-21 19:07:12 +0530

    smfe

--- src/api/UserRequest.js
+++ src/api/UserRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios" 
 
-const API = axios.create({baseURL : "https://socialmediarestapi3.onrender.com"}); 
+const API = axios.create({baseURL : "https://socialmediarestapi4.onrender.com"}); 
 API.interceptors.request.use((req)=>{
 
     if(localStorage.getItem('profile')){
